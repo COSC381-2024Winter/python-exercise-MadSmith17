bd4ea3fb829cf5d41ca8f634c7202e18859f5840
Madelyn Smith <msmit378@emich.edu>
2024-03-19 00:09:49 +0000

    User enters 1 and the program lists all the movies it has avalible, it does this by calling the list_movie-names function

--- query.py
+++ query.py
@@ -1,4 +1,5 @@
 from movies import Movies
+
 class Query:
     def __init__(self):
         self.movies = Movies()
@@ -6,14 +7,22 @@ class Query:
     def menu(self):
         while True:
             print("Menu:")
+            print("1. List all movie names")
             print("q. Quit")
             
             choice = input("Enter your choice: ")
             if choice == 'q':
                 print("Exiting program...")
                 break
+            elif choice == '1':
+                self.list_movie_names()
             else:
                 print("Invalid choice. Please try again.")
+
+    def list_movie_names(self):
+        movies = self.movies.get_movie_names()
+        for i, movie in enumerate(movies, 1):
+            print(f"{i}. {movie}")
 
 if __name__ == "__main__":
     query = Query()
