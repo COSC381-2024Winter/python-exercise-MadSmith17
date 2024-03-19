a4d1cb0530eff765462e36b92e2a2d00698743f3
Madelyn Smith <msmit378@emich.edu>
2024-03-19 01:13:37 +0000

    User enters cast and the program asks for a name of the actor/actress you are looking for and gives you all the results if there are any - Fixes #8

--- query.py
+++ query.py
@@ -7,45 +7,55 @@ class Query:
     def menu(self):
         while True:
             print("Menu:")
-            print("1. List all movie names")
-            print("2. Search movies by names")
-            print("3. Search movies by cast")
-            print("q. Quit")
+            print("list - List all movie names")
+            print("search - Search movies by names")
+            print("cast - Search movies by cast")
+            print("q - Quit")
             
             choice = input("Enter your choice: ")
             if choice == 'q':
                 print("Exiting program...")
                 break
-            elif choice == '1':
+            elif choice == "list":
                 self.list_movie_names()
-            elif choice == '2':
+            elif choice == "search":
                 self.search_by_name()
-            elif choice == '3':
+            elif choice == "cast":
                 self.search_by_cast()
             else:
                 print("Invalid choice. Please try again.")
 
     def list_movie_names(self):
-        movies = self.movies.get_movie_names()
+        movies = self.movies._movies
+        if not movies:
+            print("No movies found.")
+            return
         for i, movie in enumerate(movies, 1):
             print(f"{i}. {movie['name']}")
 
     def search_by_name(self):
-        search_term = input("Enter a partial name of the movie: ")
-        results = self.movies.search_by_name(search_term)
-        if results:
+        search_term = input("Enter a partial name of the movie: ").lower()
+        found_movies = []
+        for movie in self.movies._movies:
+            if search_term in movie['name'].lower():
+                found_movies.append(movie['name'])
+        if found_movies:
             print("Search results:")
-            for result in results:
-                print(result)
+            for movie in found_movies:
+                print(movie)
         else:
             print("No results found.")
 
     def search_by_cast(self):
         search_term = input("Enter a partial name of the cast member: ")
-        results = self.movies.search_by_cast(search_term)
-        if results:
+        found_movies = {}
+        for movie in self.movies._movies:
+            for actor in movie['cast']:
+                if search_term.lower() in actor.lower():
+                    found_movies.setdefault(movie['name'], []).append(actor)
+        if found_movies:
             print("Search results:")
-            for movie, cast in results.items():
+            for movie, cast in found_movies.items():
                 print(f"{movie}: {', '.join(cast)}")
         else:
             print("No results found.")
