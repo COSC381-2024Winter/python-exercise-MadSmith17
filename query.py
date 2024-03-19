eadf7719178e0323ebeacc7b8ea086714e576678
Madelyn Smith <msmit378@emich.edu>
2024-03-19 00:42:50 +0000

    Uesr enters 3 and the program looks for a match in the cast for the name the user entered - Fixes #8

--- query.py
+++ query.py
@@ -2,13 +2,14 @@ from movies import Movies
 
 class Query:
     def __init__(self):
-        self.movies = Movies()
+        self.movies = Movies('./movies.txt')
 
     def menu(self):
         while True:
             print("Menu:")
             print("1. List all movie names")
             print("2. Search movies by names")
+            print("3. Search movies by cast")
             print("q. Quit")
             
             choice = input("Enter your choice: ")
@@ -19,6 +20,8 @@ class Query:
                 self.list_movie_names()
             elif choice == '2':
                 self.search_by_name()
+            elif choice == '3':
+                self.search_by_cast()
             else:
                 print("Invalid choice. Please try again.")
 
@@ -34,6 +37,17 @@ class Query:
             print("Search results:")
             for result in results:
                 print(result)
+        else:
+            print("No results found.")
+
+    def search_by_cast(self):
+        search_term = input("Enter a partial name of the cast member: ")
+        results = self.movies.search_by_cast(search_term)
+        if results:
+            print("Search results:")
+            for movie, cast in results.items():
+                print(movie)
+                print(cast)
         else:
             print("No results found.")
 
